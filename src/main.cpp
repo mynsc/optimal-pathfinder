f9fbfb460213ee8a4c702f599ee14ce8d10b4068
mynsc <mcerquin.10@gmail.com>
2026-06-01 21:55:56 -0500

    refactor: streamline vertex initialization and enhance readability in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,127 +1,47 @@
 #include "arista.hpp"
+#include "grafo.hpp"
 #include "vertice.hpp"
 #include "mostar_vertice.hpp"
 #include <iostream>
 
 int main()
 {
-    // VERTICES
+    // vertices
 
-    ListaVertices listaVertices = NULL;
+    ListaVertices listaVertices = nullptr;
+    ListaVertices v1 = crearVertice("Fac. Ing. Geologica");
+    ListaVertices v2 = crearVertice("Fac. Ing. Metalurgica");
+    ListaVertices v3 = crearVertice("Fac. Quimica e Ing. Geologica");
+    ListaVertices v4 = crearVertice("Fac. Ciencias Matematicas");
+    ListaVertices v5 = crearVertice("Fac. Ciencias Biologicas");
 
-    // ARISTAS
-
-    ListaVertices v1 = new Vertice();
-    v1->nombre = "Fac. Ing. Geologica";
-
-    ListaVertices v2 = new Vertice();
-    v2->nombre = "Fac. Ing. Metalurgica";
-
-    ListaVertices v3 = new Vertice();
-    v3->nombre = "Fac. Quimica e Ing. Geologica";
-
-    ListaVertices v4 = new Vertice();
-    v4->nombre = "Fac. Ciencias Matematicas";
-
-    ListaVertices v5 = new Vertice();
-    v5->nombre = "Fac. Ciencias Biologicas";
+    // circularidad
 
     listaVertices = v1;
     v1->siguiente = v2;
     v2->siguiente = v3;
     v3->siguiente = v4;
     v4->siguiente = v5;
-    v5->siguiente = listaVertices; // CIRCULARIDAD DE LA LISTA DE VERTICES
+    v5->siguiente = listaVertices;
 
-    // a -> ARISTA , ap -> ARISTA INVERSA  (CREACION DE ARISTAS BIDIRECCIONALES) FACILITA LA NAVEGACION EN AMBOS SENTIDOS
-    ListaAristas a1 = new Arista();
-    a1->peso = 5;
-    a1->origen = v1;
-    a1->destino = v2;
+    // enlazar bidireccionalmente aristas
 
-    ListaAristas a1p = new Arista();
-    a1p->peso = 5;
-    a1p->origen = v2;
-    a1p->destino = v1;
+    enlaceBidireccional(v1, v2, 5);
+    enlaceBidireccional(v1, v5, 3);
+    enlaceBidireccional(v1, v3, 4);
+    enlaceBidireccional(v2, v4, 3);
+    enlaceBidireccional(v5, v4, 4);
+    enlaceBidireccional(v3, v4, 1);
 
-    ListaAristas a2 = new Arista();
-    a2->peso = 3;
-    a2->origen = v1;
-    a2->destino = v5;
-
-    ListaAristas a2p = new Arista();
-    a2p->peso = 3;
-    a2p->origen = v5;
-    a2p->destino = v1;
-
-    ListaAristas a3 = new Arista();
-    a3->peso = 4;
-    a3->origen = v1;
-    a3->destino = v3;
-
-    ListaAristas a3p = new Arista();
-    a3p->peso = 4;
-    a3p->origen = v3;
-    a3p->destino = v1;
-
-    ListaAristas a4 = new Arista();
-    a4->peso = 3;
-    a4->origen = v2;
-    a4->destino = v4;
-
-    ListaAristas a4p = new Arista();
-    a4p->peso = 3;
-    a4p->origen = v4;
-    a4p->destino = v2;
-
-    ListaAristas a5 = new Arista();
-    a5->peso = 4;
-    a5->origen = v5;
-    a5->destino = v4;
-
-    ListaAristas a5p = new Arista();
-    a5p->peso = 4;
-    a5p->origen = v4;
-    a5p->destino = v5;
-
-    ListaAristas a6 = new Arista();
-    a6->peso = 1;
-    a6->origen = v3;
-    a6->destino = v4;
-
-    ListaAristas a6p = new Arista();
-    a6p->peso = 1;
-    a6p->origen = v4;
-    a6p->destino = v3;
-
-    // INTEGRACION DE LAS ARISTAS A LOS VERTICES (LISTA DE ADYACENCIA)
-    v1->AristaAdyacente = a1;
-    a1->siguiente = a2;
-    a2->siguiente = a3;
-
-    v2->AristaAdyacente = a1p;
-    a1p->siguiente = a4;
-
-    v3->AristaAdyacente = a3p;
-    a3p->siguiente = a6;
-
-    v4->AristaAdyacente = a4p;
-    a4p->siguiente = a5p;
-    a5p->siguiente = a6p;
-
-    v5->AristaAdyacente = a2p;
-    a2p->siguiente = a5;
-
-    // VER RUTAS Y PESOS DE CADA VERTICE DE VERTICE EN ESPECIFICO : PRUEBA
+    // ver rutas y pesos de cada vertice en especifico
 
     ListaVertices copia = listaVertices;
-    int op;
+    int opcion  = 0;
     do
     {
         //system("cls");
 
-        std::cout << "\tINGRESE VERTICE A VER SUS VECINOS:\t\n"
-                  << std::endl;
+        std::cout << "\tINGRESE VERTICE A VER SUS VECINOS:\t\n\n";
         std::cout << "presionar 0 para salir\n " << std::endl;
         int i = 1;
         do
@@ -131,10 +51,10 @@ int main()
             i++;
 
         } while (copia != listaVertices);
-        std::cout << "\ningrese opcion -----> ";
-        std::cin >> op;
+        std::cout << "\nIngrese opcion >> ";
+        std::cin >> opcion;
 
-        switch (op)
+        switch (opcion)
         {
         case 1:
             mostrarVertices(v1);
@@ -152,12 +72,13 @@ int main()
             mostrarVertices(v5);
             break;
         case 0:
+            std::cout << "Saliendo del programa";
             break;
         default:
-            std::cout << "OPCION INVALIDA" << std::endl;
+            std::cout << "Opcion invalida\n";
         }
         //system("pause");
-    } while (op != 0);
+    } while (opcion != 0);
 
     return 0;
 }
