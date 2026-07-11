f331677d7d82db5455af3e18403902012868027d
mynsc <mcerquin.10@gmail.com>
2026-07-10 21:17:29 -0500

    agregar logo como icono de ventana y carga de textura del mapa

--- src/interfaz.cpp
+++ src/interfaz.cpp
@@ -6,7 +6,29 @@
 void inicializarVentana()
 {
     // Crear la ventana principal
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Optimal PathFinder - UNMSM");
+    sf::RenderWindow window(sf::VideoMode({389, 487}), "Optimal PathFinder - UNMSM");
+
+    // Cargar la imagen del icono
+    sf::Image icono;
+    if (!icono.loadFromFile("assets/logo.png")) return;
+
+    // Establecer el icono en la ventana
+    window.setIcon(icono.getSize(), icono.getPixelsPtr());
+
+    // Cargar la textura del mapa desde un archivo
+    sf::Texture texturaMapa;
+    if (!texturaMapa.loadFromFile("assets/mapa.png")) return;
+
+    // Crear el sprite del mapa y asignarle la textura
+    sf::Sprite spriteMapa(texturaMapa);
+
+    // Escalar el sprite del mapa para que cubra toda la ventana
+    sf::Vector2u windowSize = window.getSize();
+    sf::Vector2u textureSize = texturaMapa.getSize();
+
+    // Calculos matematicos para la escala de la textura
+    float scaleX = static_cast<float>(windowSize.x) / textureSize.x;
+    float scaleY = static_cast<float>(windowSize.y) / textureSize.y;
 
     // Iniciar el bucle de la ventana
     while (window.isOpen())
@@ -22,6 +44,8 @@ void inicializarVentana()
         // Limpiar ventana
         window.clear();
 
+        window.draw(spriteMapa);
+        
         // Actualizar ventana
         window.display();
     }
